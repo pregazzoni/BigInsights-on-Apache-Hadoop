7535cca1af1fa540287bda3ab36d96ba066473c5
Chris Snow <chsnow123@gmail.com>
2016-07-22 16:56:32 +0100

    Update Example.groovy

--- examples/OozieWorkflowMapReduce/Example.groovy
+++ examples/OozieWorkflowMapReduce/Example.groovy
@@ -28,10 +28,16 @@ gateway = env.gateway
 username = env.username
 password = env.password
 
+// the Apache 2.0 LICENSE filename that we send to the cluster for wordcount analysis
 inputFile = "LICENSE"
+
+// we will use this folder name in HDFS for storing the Oozie inputs and outputs
 jobDir = "/user/" + username + "/test"
+
+// the name of the jar file containing the Map/Reduce wordcount code
 jarFile = "samples/hadoop-examples.jar"
 
+// we need to create an Oozie workflow XML document
 definition = """\
 <workflow-app xmlns="uri:oozie:workflow:0.2" name="wordcount-workflow">
     <start to="root-node"/>
@@ -53,6 +59,7 @@ definition = """\
 </workflow-app>
 """
 
+// we need to create an Oozie configuration XML document
 configuration = """\
 <configuration>
     <property>
@@ -82,22 +89,30 @@ configuration = """\
 </configuration>
 """
 
+// login to the Knox REST API
 session = Hadoop.login( gateway, username, password )
 
+// make sure the HDFS jobDir is clean - first delete it recursively, then create a new folder
+// See example/WebHdfsGroovy for more information on these commands
 println "[Example.groovy] Delete " + jobDir + ": " + Hdfs.rm( session ).file( jobDir ).recursive().now().statusCode
 println "[Example.groovy] Mkdir " + jobDir + ": " + Hdfs.mkdir( session ).dir( jobDir ).now().statusCode
 
+// upload the Apache 2.0 LICENSE file
 putData = Hdfs.put(session).file( inputFile ).to( jobDir + "/input/FILE" ).later() {
   println "[Example.groovy] Put " + jobDir + "/input/FILE: " + it.statusCode }
 
+// upload the jar file with the Map/Reduce code
 putJar = Hdfs.put(session).file( jarFile ).to( jobDir + "/lib/hadoop-examples.jar" ).later() {
   println "[Example.groovy] Put " + jobDir + "/lib/hadoop-examples.jar: " + it.statusCode }
 
+// upload the Oozie workflow xml 
 putWorkflow = Hdfs.put(session).text( definition ).to( jobDir + "/workflow.xml" ).later() {
   println "[Example.groovy] Put " + jobDir + "/workflow.xml: " + it.statusCode }
 
+// wait for the files to get uploaded
 session.waitFor( putWorkflow, putData, putJar )
 
+// submit the Oozie configuration  - this starts the Oozie job
 jobId = Workflow.submit(session).text( configuration ).now().jobId
 println "[Example.groovy] Submitted job: " + jobId
 
@@ -105,6 +120,7 @@ println "[Example.groovy] Polling up to 60s for job completion..."
 status = "RUNNING";
 count = 0;
 
+// Poll the server every second to check the job status
 print "[Example.groovy] "
 while( status == "RUNNING" && count++ < 60 ) {
   sleep( 1000 )
@@ -116,14 +132,19 @@ println ""
 println "[Example.groovy] Job status: " + status
 
 if( status == "SUCCEEDED" ) {
+  // retrieve the output from the job that has been put in the HDFS folder
   text = Hdfs.ls( session ).dir( jobDir + "/output" ).now().string
+  
+  // get the job summary output
   json = (new JsonSlurper()).parseText( text )
   println "[Example.groovy] " + json.FileStatuses.FileStatus.pathSuffix
 
+  // get the wordcount output
   println "[Example.groovy] Mapreduce output:"
   println '*' * 80
   println Hdfs.get( session ).from( jobDir + "/output/part-r-00000" ).now().string
   println '*' * 80
 }
 
+// close the session with the REST API
 session.shutdown()
